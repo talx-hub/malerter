508220ef420c993db0414c77c6e81bc8e0b35447
Alexey Titaev <alx.titaev@yandex.ru>
2024-10-03 21:49:51 +0300

    add dummy service

---
+++ internal/service/service.go
@@ -0,0 +1,34 @@
+package service
+
+import "github.com/alant1t/metricscoll/internal/repo"
+
+type Service interface {
+	DumpMetric(string) error
+}
+
+type MetricsDumper struct {
+	repo repo.Repository
+}
+
+func NewMetricsDumper(repo repo.Repository) *MetricsDumper {
+	// почему тут возвращаем адрес?
+	return &MetricsDumper{repo: repo}
+}
+
+func (d *MetricsDumper) DumpMetric(rawMetric string) error {
+	var metric string
+	var err error
+	if metric, err = parseURL(rawMetric); err != nil {
+		return err
+	}
+
+	if err = d.repo.Store(metric); err != nil {
+		return err
+	}
+
+	return nil
+}
+
+func parseURL(metricURL string) (string, error) {
+	return metricURL, nil
+}
