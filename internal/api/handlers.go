50d833db5e9fbd44308e2a21ecf467b0f162873c
Alexey Titaev <alx.titaev@yandex.ru>
2024-10-04 00:15:25 +0300

    add dummy handler

---
+++ internal/api/handlers.go
@@ -0,0 +1,47 @@
+package api
+
+import (
+	"github.com/alant1t/metricscoll/internal/customerror"
+	"github.com/alant1t/metricscoll/internal/service"
+	"net/http"
+)
+
+// Зачем мы создаем свою структуру хэндлера?
+// Почему просто не сделать пакет с экспортируемыми
+// функциями?
+type HTTPHandler struct {
+	service service.Service
+}
+
+func NewHTTPHandler(service service.Service) *HTTPHandler {
+	return &HTTPHandler{service: service}
+}
+
+func (h *HTTPHandler) DumpMetric(w http.ResponseWriter, r *http.Request) {
+	if r.Method != http.MethodPost {
+		e := "Only POST requests are allowed"
+		http.Error(w, e, http.StatusBadRequest)
+		return
+	}
+
+	if r.Header.Get("Content-Type") != "text/plain" {
+		e := "Content-Type must be \"text/plain\""
+		http.Error(w, e, http.StatusBadRequest)
+		return
+	}
+
+	rawMetric := r.URL.RawQuery
+	if err := h.service.DumpMetric(rawMetric); err != nil {
+		switch err.(type) {
+		case *customerror.NotFoundError:
+			http.Error(w, err.Error(), http.StatusNotFound)
+		case *customerror.WrongTypeError:
+			http.Error(w, err.Error(), http.StatusBadRequest)
+		default:
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+}
--- internal/customerror/error.go
+++ internal/customerror/error.go
@@ -9,3 +9,11 @@ type NotFoundError struct {
 func (e *NotFoundError) Error() string {
 	return fmt.Sprintf("Metric %v not found", e.Metric)
 }
+
+type WrongTypeError struct {
+	RawMetric string
+}
+
+func (e *WrongTypeError) Error() string {
+	return fmt.Sprintf("Raw metric %v is incorrect", e.RawMetric)
+}
