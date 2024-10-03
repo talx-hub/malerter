8a704ea06cb90d3f222f4e8a24f02486366c5d5f
Alexey Titaev <alx.titaev@yandex.ru>
2024-10-04 00:46:38 +0300

    add dummy server

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,3 +1,24 @@
 package main
 
-func main() {}
+import (
+	"github.com/alant1t/metricscoll/internal/api"
+	"github.com/alant1t/metricscoll/internal/repo"
+	"github.com/alant1t/metricscoll/internal/service"
+	"net/http"
+)
+
+func main() {
+	rep := repo.MemRepository{}
+	serv := service.NewMetricsDumper(&rep)
+	handler := api.NewHTTPHandler(serv)
+
+	var updateHandler http.Handler = http.HandlerFunc(handler.DumpMetric)
+
+	mux := http.NewServeMux()
+	mux.Handle("/update/", updateHandler)
+
+	err := http.ListenAndServe(":8080", mux)
+	if err != nil {
+		panic(err)
+	}
+}
