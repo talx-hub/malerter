54f966d9e54e44820e3ce93f9dee8d9246ae8321
Alexey Titaev <alx.titaev@yandex.ru>
2024-10-05 22:38:15 +0300

    add agent run code

--- cmd/agent/main.go
+++ cmd/agent/main.go
@@ -1,3 +1,31 @@
 package main
 
-func main() {}
+import (
+	"github.com/alant1t/metricscoll/internal/repo"
+	"github.com/alant1t/metricscoll/internal/service"
+	"time"
+)
+
+// TODO: сделать клиент модульным:
+//		- модуль сбора метрик
+//		- модуль отправки метрик
+//		- первый модуль собирает метрики
+//		- затем оповещает модуль отпраки, что данные готовы
+//		- но как сделать нотификацию???
+
+func main() {
+	rep := repo.NewMemRepository()
+	agent := service.NewAgent(rep)
+
+	var i = 0
+	for {
+		agent.Update()
+
+		if i%5 == 0 {
+			agent.Send()
+			i = 0
+		}
+		i++
+		time.Sleep(2 * time.Second)
+	}
+}
