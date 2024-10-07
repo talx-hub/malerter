53955f3a41486e6e9f0fb78e7175926162bd8b9b
Alexey Titaev <alx.titaev@yandex.ru>
2024-10-07 22:58:19 +0300

    refactor test

--- internal/api/handlers_test.go
+++ internal/api/handlers_test.go
@@ -12,122 +12,6 @@ import (
 	"testing"
 )
 
-func TestHTTPHandler_DumpMetric(t *testing.T) {
-	type want struct {
-		code        int
-		response    string
-		contentType string
-	}
-	tests := []struct {
-		name   string
-		method string
-		reqURL string
-		want   want
-	}{
-		{
-			name:   "positive test #1: counter",
-			method: http.MethodPost,
-			reqURL: "/update/counter/someMetric/123",
-			want: want{
-				code:     200,
-				response: "",
-			},
-		},
-		{
-			name:   "positive test #2: gauge",
-			method: http.MethodPost,
-			reqURL: "/update/gauge/someMetric/123.1",
-			want: want{
-				code:     200,
-				response: "",
-			},
-		},
-		{
-			name:   "positive test #3: int gauge",
-			method: http.MethodPost,
-			reqURL: "/update/gauge/someMetric/1",
-			want: want{
-				code:     200,
-				response: "",
-			},
-		},
-		{
-			name:   "negative test #1: wrong method",
-			method: http.MethodGet,
-			reqURL: "/update/gauge/someMetric/1",
-			want: want{
-				code:     400,
-				response: "only POST requests are allowed\n",
-			},
-		},
-		{
-			name:   "negative test #2: no metric name",
-			method: http.MethodPost,
-			reqURL: "/update/gauge/1",
-			want: want{
-				code:     404,
-				response: "metric /update/gauge/1 not found\n",
-			},
-		},
-		{
-			name:   "negative test #3: wrong metric type",
-			method: http.MethodPost,
-			reqURL: "/update/WRONG/someMetric/1",
-			want: want{
-				code:     400,
-				response: "metric /update/WRONG/someMetric/1 is incorrect\n",
-			},
-		},
-		{
-			name:   "negative test #4: wrong value type",
-			method: http.MethodPost,
-			reqURL: "/update/counter/someMetric/1.0",
-			want: want{
-				code:     400,
-				response: "metric /update/counter/someMetric/1.0 is incorrect\n",
-			},
-		},
-		{
-			name:   "negative test #5: no value",
-			method: http.MethodPost,
-			reqURL: "/update/counter/someMetric",
-			want: want{
-				code:     404,
-				response: "metric /update/counter/someMetric not found\n",
-			},
-		},
-		{
-			name:   "negative test #6: value overflow",
-			method: http.MethodPost,
-			reqURL: "/update/counter/someMetric/9223372036854775808",
-			want: want{
-				code:     400,
-				response: "metric /update/counter/someMetric/9223372036854775808 is incorrect\n",
-			},
-		},
-	}
-	for _, tt := range tests {
-		t.Run(tt.name, func(t *testing.T) {
-			request := httptest.NewRequest(tt.method, tt.reqURL, nil)
-			w := httptest.NewRecorder()
-
-			rep := repo.NewMemRepository()
-			serv := service.NewMetricsDumper(rep)
-			handler := NewHTTPHandler(serv)
-
-			handler.DumpMetric(w, request)
-			res := w.Result()
-
-			assert.Equal(t, tt.want.code, res.StatusCode)
-			defer res.Body.Close()
-			resBody, err := io.ReadAll(res.Body)
-
-			require.NoError(t, err)
-			assert.Equal(t, tt.want.response, string(resBody))
-		})
-	}
-}
-
 func TestNewHTTPHandler(t *testing.T) {
 	type args struct {
 		service service.Service
@@ -160,4 +44,64 @@ func TestNewHTTPHandler(t *testing.T) {
 			}
 		})
 	}
+}
+
+type test struct {
+	url    string
+	want   string
+	status int
+}
+
+func TestHTTPHandler_DumpMetric(t *testing.T) {
+	tests := []test{
+		{"/update/counter/someMetric/123", "", 200},
+		{"/update/gauge/someMetric/123.1", "", 200},
+		{"/update/gauge/someMetric/1", "", 200},
+		{"/update/gauge/1", "metric /update/gauge/1 not found\n", 404},
+		{"/update/WRONG/someMetric/1", "metric /update/WRONG/someMetric/1 is incorrect\n", 400},
+		{"/update/counter/someMetric/1.0", "metric /update/counter/someMetric/1.0 is incorrect\n", 400},
+		{"/update/counter/someMetric", "metric /update/counter/someMetric not found\n", 404},
+		{"/update/counter/someMetric/9223372036854775808", "metric /update/counter/someMetric/9223372036854775808 is incorrect\n", 400},
+		{"/update/counter/someMetric/string", "metric /update/counter/someMetric/string is incorrect\n", 400},
+	}
+	rep := repo.NewMemRepository()
+	serv := service.NewMetricsDumper(rep)
+	handler := NewHTTPHandler(serv)
+	ts := httptest.NewServer(http.HandlerFunc(handler.DumpMetric))
+	defer ts.Close()
+
+	wrongMethodTest := test{
+		url:    "/update/gauge/someMetric/1",
+		want:   "only POST requests are allowed\n",
+		status: 400,
+	}
+	t.Run("wrong method test", func(t *testing.T) {
+		resp, body := testRequest(t, ts, http.MethodGet, wrongMethodTest.url)
+		assert.Equal(t, wrongMethodTest.status, resp.StatusCode)
+		assert.Equal(t, wrongMethodTest.want, body)
+	})
+
+	for _, tt := range tests {
+		t.Run(tt.url, func(t *testing.T) {
+			resp, body := testRequest(t, ts, http.MethodPost, tt.url)
+			assert.Equal(t, tt.status, resp.StatusCode)
+			assert.Equal(t, tt.want, body)
+		})
+	}
+}
+
+func testRequest(t *testing.T, ts *httptest.Server,
+	method, path string) (*http.Response, string) {
+
+	request, err := http.NewRequest(method, ts.URL+path, nil)
+	require.NoError(t, err)
+
+	resp, err := ts.Client().Do(request)
+	require.NoError(t, err)
+
+	body, err := io.ReadAll(resp.Body)
+	require.NoError(t, err)
+	defer resp.Body.Close()
+
+	return resp, string(body)
 }
