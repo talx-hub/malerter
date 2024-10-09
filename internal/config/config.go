e0b40fd0b5dfcceb4e7a0b19ac5da6e086a8ee20
Alexey Titaev <alx.titaev@yandex.ru>
2024-10-09 18:19:09 +0300

    refactor: replace hardcode to consts

--- internal/config/config.go
+++ internal/config/config.go
@@ -12,13 +12,25 @@ type ServerConfig struct {
 	RootAddress string
 }
 
+const (
+	hostDefault           = "localhost:8080"
+	reportIntervalDefault = 10
+	poolIntervalDefault   = 2
+)
+
+const (
+	envAddress        = "ADDRESS"
+	envReportInterval = "REPORT_INTERVAL"
+	envPollInterval   = "POLL_INTERVAL"
+)
+
 func LoadServerConfig() *ServerConfig {
 	var config ServerConfig
-	flag.StringVar(&config.RootAddress, "a", "localhost:8080",
+	flag.StringVar(&config.RootAddress, "a", hostDefault,
 		"server root address")
 	flag.Parse()
 
-	if a, found := os.LookupEnv("ADDRESS"); found {
+	if a, found := os.LookupEnv(envAddress); found {
 		config.RootAddress = a
 	}
 
@@ -40,16 +52,16 @@ func LoadAgentConfig() *AgentConfig {
 }
 
 func loadAgentCLConfig(config *AgentConfig) {
-	flag.StringVar(&config.ServerAddress, "a", "localhost:8080",
-		"alert server address")
+	flag.StringVar(&config.ServerAddress, "a", hostDefault,
+		"alert host address")
 
-	flag.DurationVar(&config.ReportInterval, "r", 10,
+	flag.DurationVar(&config.ReportInterval, "r", reportIntervalDefault,
 		"interval in seconds of sending metrics to alert server")
 	if config.ReportInterval < 0 {
 		log.Fatal("report interval must be positive")
 	}
 
-	flag.DurationVar(&config.PollInterval, "p", 2,
+	flag.DurationVar(&config.PollInterval, "p", poolIntervalDefault,
 		"interval in seconds of polling and metric collection")
 	if config.PollInterval < 0 {
 		log.Fatal("poll interval must be positive")
@@ -59,10 +71,10 @@ func loadAgentCLConfig(config *AgentConfig) {
 }
 
 func loadAgentEnvConfig(config *AgentConfig) {
-	if a, found := os.LookupEnv("ADDRESS"); found {
+	if a, found := os.LookupEnv(envAddress); found {
 		config.ServerAddress = a
 	}
-	if ri, found := os.LookupEnv("REPORT_INTERVAL"); found {
+	if ri, found := os.LookupEnv(envReportInterval); found {
 		riInt, err := strconv.Atoi(ri)
 		if err != nil {
 			log.Fatal(err)
@@ -72,7 +84,7 @@ func loadAgentEnvConfig(config *AgentConfig) {
 		}
 		config.ReportInterval = time.Duration(riInt)
 	}
-	if rp, found := os.LookupEnv("POLL_INTERVAL"); found {
+	if rp, found := os.LookupEnv(envPollInterval); found {
 		rpInt, err := strconv.Atoi(rp)
 		if err != nil {
 			log.Fatal(err)
