4d104a1e9e75ca399a7fc837c8af3aa9308a26a9
Alexey Titaev <alx.titaev@yandex.ru>
2024-11-06 22:50:39 +0300

    refactor(errors): add info to custom errors

--- internal/customerror/error.go
+++ internal/customerror/error.go
@@ -3,17 +3,19 @@ package customerror
 import "fmt"
 
 type NotFoundError struct {
-	RawMetric string
+	MetricURL string
+	Info      string
 }
 
 func (e *NotFoundError) Error() string {
-	return fmt.Sprintf("metric %v not found", e.RawMetric)
+	return fmt.Sprintf("metric %v not found: %s", e.MetricURL, e.Info)
 }
 
 type InvalidArgumentError struct {
-	RawMetric string
+	MetricURL string
+	Info      string
 }
 
 func (e *InvalidArgumentError) Error() string {
-	return fmt.Sprintf("metric %v is incorrect", e.RawMetric)
+	return fmt.Sprintf("metric %v is incorrect: %s", e.MetricURL, e.Info)
 }
