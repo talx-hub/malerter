b75f6eeaf71e39f86f362f399b3c89814f4ee4f4
Alexey Titaev <alx.titaev@yandex.ru>
2024-11-14 18:19:31 +0300

    feat(server): add logging and backup configuration

--- internal/config/server/builder.go
+++ internal/config/server/builder.go
@@ -2,18 +2,33 @@ package server
 
 import (
 	"flag"
+	"fmt"
+	"log"
 	"os"
+	"strconv"
+	"time"
 
 	"github.com/talx-hub/malerter/internal/config"
 )
 
 const (
-	AddressDefault = "localhost:8080"
+	AddressDefault       = "localhost:8080"
+	LogLevelDefault      = "InfoLevel"
+	StoreIntervalDefault = 300
+	RestoreDefault       = true
 )
 
 const (
-	EnvAddress = "ADDRESS"
+	EnvAddress         = "ADDRESS"
+	EnvLogLevel        = "LOG_LEVEL"
+	EnvStoreInterval   = "STORE_INTERVAL"
+	EnvFileStoragePath = "FILE_STORAGE_PATH"
+	EnvRestore         = "RESTORE"
 )
+
+func FileStorageDefault() string {
+	return time.Now().UTC().String() + ".bk"
+}
 
 func NewDirector() *config.Director {
 	return &config.Director{
@@ -22,12 +37,23 @@ func NewDirector() *config.Director {
 }
 
 type Builder struct {
-	RootAddress string
+	RootAddress     string
+	LogLevel        string
+	FileStoragePath string
+	StoreInterval   time.Duration
+	Restore         bool
 }
 
 func (b *Builder) LoadFromFlags() config.Builder {
 	flag.StringVar(&b.RootAddress, "a", AddressDefault, "server root address")
+	flag.StringVar(&b.LogLevel, "l", LogLevelDefault, "server log level")
+	flag.StringVar(&b.FileStoragePath, "f", FileStorageDefault(), "backup file path")
+	var backupInterval int64
+	flag.Int64Var(&backupInterval, "i", StoreIntervalDefault, "interval in seconds of repository backup")
+	b.Restore = *flag.Bool("r", RestoreDefault, "restore backup while start")
 	flag.Parse()
+
+	b.StoreInterval = time.Duration(backupInterval) * time.Second
 	return b
 }
 
@@ -35,10 +61,33 @@ func (b *Builder) LoadFromEnv() config.Builder {
 	if a, found := os.LookupEnv(EnvAddress); found {
 		b.RootAddress = a
 	}
+	if l, found := os.LookupEnv(EnvLogLevel); found {
+		b.LogLevel = l
+	}
+	if f, found := os.LookupEnv(EnvFileStoragePath); found {
+		b.FileStoragePath = f
+	}
+	if i, found := os.LookupEnv(EnvStoreInterval); found {
+		backupInterval, err := strconv.Atoi(i)
+		if err != nil {
+			log.Fatal(err)
+		}
+		b.StoreInterval = time.Duration(backupInterval) * time.Second
+	}
+	if r, found := os.LookupEnv(EnvRestore); found {
+		var err error
+		b.Restore, err = strconv.ParseBool(r)
+		if err != nil {
+			log.Fatal(err)
+		}
+	}
 	return b
 }
 
 func (b *Builder) IsValid() (config.Builder, error) {
+	if b.StoreInterval < 0 {
+		return nil, fmt.Errorf("store interval must be positive")
+	}
 	return b, nil
 }
 
