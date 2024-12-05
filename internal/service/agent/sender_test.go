3ae62a53ebc9dfdbfef31e4776fc8b34b782b87e
Alexey Titaev <alx.titaev@yandex.ru>
2024-12-06 01:18:29 +0300

    fix(tests): agent batch send

--- internal/service/agent/sender_test.go
+++ internal/service/agent/sender_test.go
@@ -64,28 +64,41 @@ func TestConvertToJSONs(t *testing.T) {
 
 func TestSend(t *testing.T) {
 	tests := []struct {
-		name  string
-		jsons []string
+		name    string
+		metrics []model.Metric
+		want    string
 	}{
 		{
-			name:  "empty",
-			jsons: []string{},
+			name:    "empty",
+			metrics: []model.Metric{},
+			want:    "[]",
 		},
 		{
 			name: "no error",
-			jsons: []string{
-				`{"id":"m42","type":"counter","delta":42}`,
-				`{"id":"pi","type":"gauge","value":3.14}`,
+			metrics: []model.Metric{
+				{
+					Delta: func(i int64) *int64 { return &i }(42),
+					Value: nil,
+					Type:  "counter",
+					Name:  "m42",
+				},
+				{
+					Delta: nil,
+					Value: func(i float64) *float64 { return &i }(3.14),
+					Type:  "gauge",
+					Name:  "pi",
+				},
 			},
+			want: `[{"id":"m42","type":"counter","delta":42},{"id":"pi","type":"gauge","value":3.14}]`,
 		},
 	}
 	// FIXME: эта городуха вообще норм? :DDD
-	storage := make([]string, 0)
+	storage := ""
 	testServer := httptest.NewServer(http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		if body, err := io.ReadAll(r.Body); err != nil {
 			log.Println(err)
 		} else {
-			storage = append(storage, string(body))
+			storage = string(body)
 		}
 		if err := r.Body.Close(); err != nil {
 			log.Println(err)
@@ -94,13 +107,20 @@ func TestSend(t *testing.T) {
 	defer testServer.Close()
 
 	client := testServer.Client()
+	sender := Sender{
+		client:   client,
+		storage:  memory.New(),
+		host:     testServer.URL,
+		compress: false,
+	}
 	for _, test := range tests {
 		t.Run(test.name, func(t *testing.T) {
-			send(client, testServer.URL, test.jsons, false)
-			require.Equal(t, len(test.jsons), len(storage))
-			for i, json := range test.jsons {
-				assert.JSONEq(t, json, storage[i])
+			for _, m := range test.metrics {
+				err := sender.storage.Add(context.TODO(), m)
+				require.NoError(t, err)
 			}
+			sender.send()
+			assert.JSONEq(t, test.want, storage)
 		})
 	}
 }
